29e1ead44373b573eaf5edeedcd0163ae7a66aa0
Fulgrim <ivanyuschin.work@yandex.ru>
2024-03-29 21:13:53 +0300

    feat: удалить лишние пакетные зависимости

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
-import "core-js/features/map"
-import "core-js/features/set"
-import React from "react"
-import ReactDOM from "react-dom"
-import bridge from "@vkontakte/vk-bridge"
-import App from "./App"
+import "core-js/features/map";
+import "core-js/features/set";
+import React from "react";
+import ReactDOM from "react-dom";
+import bridge from "@vkontakte/vk-bridge";
+import App from "./App";
+import { AdaptivityProvider, ConfigProvider } from "@vkontakte/vkui";
 
-bridge.send("VKWebAppInit")
+bridge.send("VKWebAppInit");
 
-ReactDOM.render(<App />, document.getElementById("root"))
+const container = document.getElementById("root");
+
+const root = ReactDOM.createRoot(container);
+
+root.render(
+  <React.StrictMode>
+    <ConfigProvider appearance="dark">
+      <AdaptivityProvider>
+        <App />
+      </AdaptivityProvider>
+    </ConfigProvider>
+  </React.StrictMode>
+);
--- src/panels/Start_1/index.js
+++ src/panels/Start_1/index.js
@@ -1,58 +1,38 @@
-import React from 'react'
-import './style.css'
-import {
-  Div,
-  Button,
-  PanelHeader,
-  Panel
-} from '@vkontakte/vkui'
-import donorLogo from '../../img/DonorSearch.png'
-import puls from '../../img/puls.png'
+import React from "react";
+import "./style.css";
+import { Div, Button, PanelHeader, Panel } from "@vkontakte/vkui";
+import donorLogo from "../../img/DonorSearch.png";
+import puls from "../../img/puls.png";
 
 const Home = (props) => {
   return (
-    <Panel
-      separator={false}
-      id={props.id}
-      className='background'
-    >
-      <PanelHeader></PanelHeader>
-      <Div className='center'>
-        <div className='start_top'>
-          <img className='donorLogo' src={donorLogo} alt='donor Logo' />
-          <img className='puslLogo' src={puls} alt='donor Logo' />
-          {/* <span className='start_title'>
-            Привет, {props.fetchedUser.first_name}
-          </span> */}
-          <span
-            className='start_text'
-            style={{ marginBottom: 0 }}
-          >
-            15 августа пройдёт первый фестиваль донорского движения "Пульсация" в Казани.
-            {/* 14 июня — Всемирный день<br />
-            донора крови. */}
+    <Panel separator={false} id={props.id} className="background">
+      <PanelHeader separator={false}>
+        <img className="donorLogo" src={donorLogo} alt="donor Logo" />
+      </PanelHeader>
+      <Div className="center">
+        <div className="start_top">
+          <img className="puslLogo" src={puls} alt="donor Logo" />
+          <span className="start_text" style={{ marginBottom: 0 }}>
+            15 августа пройдёт первый фестиваль донорского движения "Пульсация"
+            в Казани.
           </span>
         </div>
-        <span className='start_text'>
-          Вокруг донорства существует множество мифов - давай узнаем, сможешь ли ты их разоблачить?"
+        <span className="start_text">
+          Вокруг донорства существует множество мифов - давай узнаем, сможешь ли
+          ты их разоблачить?"
         </span>
-        {/* <span className='start_text'>
-          Давай узнаем, сможешь ли ты их разоблачить?
-        </span> */}
-        
-        <div className='start_bottom'>
-          <Button
-            className='start_button'
-            onClick={() => {
-              props.setActivePanel('home')
-            }}
-          >
-            <b>Начать</b>
-          </Button>
-        </div>
+        <Button
+          className="start_button"
+          onClick={() => {
+            props.setActivePanel("home");
+          }}
+        >
+          Начать
+        </Button>
       </Div>
     </Panel>
-  )
-}
+  );
+};
 
 export default Home;
